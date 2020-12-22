14a356f08d07041e69180894a0ee3444202bb52b
xieli <2524546110@qq.com>
2020-12-22 21:30:39 +0800

    发送邮件

--- src/test/java/com/xieli/community/MailTests.java
+++ src/test/java/com/xieli/community/MailTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 /**
  * Created by xieli on 2020/12/22.
@@ -17,9 +19,23 @@ public class MailTests {
     @Autowired
     private MailClient mailClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void testTextMail() {
         mailClient.sendMail("2524546110@qq.com", "Test", "这不是偶然");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "xieli");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("2524546110@qq.com", "HTML", content);
     }
 
 }
