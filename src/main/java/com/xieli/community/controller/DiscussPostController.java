e7fe1a65aedece7510fc2fbae47ed636f33747d6
xieli <2524546110@qq.com>
2020-12-31 19:55:39 +0800

    帖子详情

--- src/main/java/com/xieli/community/controller/DiscussPostController.java
+++ src/main/java/com/xieli/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.xieli.community.controller;
 import com.xieli.community.entity.DiscussPost;
 import com.xieli.community.entity.User;
 import com.xieli.community.service.DiscussPostService;
+import com.xieli.community.service.UserService;
 import com.xieli.community.util.CommunityUtil;
 import com.xieli.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,15 +30,18 @@ public class DiscussPostController {
     @Autowired
     private HostHolder hostHolder;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
     public String addDiscussPost(String title, String content) {
         User user = hostHolder.getUser();
         if (user == null) {
-            return CommunityUtil.getJSONString(403,"未登录！");
+            return CommunityUtil.getJSONString(403, "未登录！");
         }
 
-        DiscussPost post=new DiscussPost();
+        DiscussPost post = new DiscussPost();
         post.setUserId(user.getId());
         post.setTitle(title);
         post.setContent(content);
@@ -43,6 +49,19 @@ public class DiscussPostController {
         discussPostService.addDiscussPost(post);
 
         // 报错的情况，将来统一处理
-        return CommunityUtil.getJSONString(0,"发布成功！");
+        return CommunityUtil.getJSONString(0, "发布成功！");
     }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post",post);
+        // 作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user",user);
+
+        return "/site/discuss-detail";
+    }
+
 }
