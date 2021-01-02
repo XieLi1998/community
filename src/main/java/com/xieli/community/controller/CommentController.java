9d1887321038a373be0299e4a3a12d93822eb792
xieli <2524546110@qq.com>
2021-01-02 17:27:43 +0800

    添加评论

---
+++ src/main/java/com/xieli/community/controller/CommentController.java
@@ -0,0 +1,38 @@
+package com.xieli.community.controller;
+
+import com.xieli.community.entity.Comment;
+import com.xieli.community.service.CommentService;
+import com.xieli.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+/**
+ * Created by xieli on 2021/1/2.
+ */
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+
+}
--- src/main/java/com/xieli/community/dao/DiscussPostMapper.java
+++ src/main/java/com/xieli/community/dao/DiscussPostMapper.java
@@ -23,6 +23,6 @@ public interface DiscussPostMapper {
 
     DiscussPost selectDiscussPostById(int id);
 
-
+    int updateCommentCount(int id, int commentCount);
 
 }
