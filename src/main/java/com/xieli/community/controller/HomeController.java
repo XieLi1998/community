bf42c893d0f865e46677581f12c7176341fdabe9
xieli <2524546110@qq.com>
2021-01-06 22:53:30 +0800

    统一异常处理

--- src/main/java/com/xieli/community/controller/HomeController.java
+++ src/main/java/com/xieli/community/controller/HomeController.java
@@ -54,6 +54,11 @@ public class HomeController {
         return "/index";
     }
 
+    @RequestMapping(path = "/error",method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
+    }
+
     // ajax示例
     @RequestMapping(path = "/ajax", method = RequestMethod.POST)
     @ResponseBody
