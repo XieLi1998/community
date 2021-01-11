61d5dc56e4c60ee3a98160a8150acb86ebe31565
xieli <2524546110@qq.com>
2021-01-11 23:54:22 +0800

    关注、取消关注

---
+++ src/main/java/com/xieli/community/controller/FollowController.java
@@ -0,0 +1,47 @@
+package com.xieli.community.controller;
+
+import com.xieli.community.entity.User;
+import com.xieli.community.service.FollowService;
+import com.xieli.community.util.CommunityUtil;
+import com.xieli.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by xieli on 2021/1/11.
+ */
+@Controller
+public class FollowController {
+
+    @Autowired
+    private FollowService followService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/follow", method = RequestMethod.POST)
+    @ResponseBody
+    public String follow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.follow(user.getId(),entityType, entityId);
+
+        return CommunityUtil.getJSONString(0,"已关注！");
+    }
+
+    @RequestMapping(path = "/unfollow", method = RequestMethod.GET)
+    @ResponseBody
+    public String unfollow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.unfollow(user.getId(),entityType, entityId);
+
+        return CommunityUtil.getJSONString(0,"已取消关注！");
+    }
+
+
+
+}
--- src/main/java/com/xieli/community/util/RedisKeyUtil.java
+++ src/main/java/com/xieli/community/util/RedisKeyUtil.java
@@ -9,6 +9,8 @@ public class RedisKeyUtil {
     public static final String SPLIT = ":";
     public static final String PREFIX_ENTITY_LIKE = "like:entity";
     public static final String PREFIX_USER_LIKE = "like:user";
+    public static final String PREFIX_FOLLOWEE = "followee";
+    public static final String PREFIX_FOLLOWER = "follower";
 
     // 某个实体的赞
     // like:entity:entityType:entityId
@@ -20,6 +22,18 @@ public class RedisKeyUtil {
     // like:user:userId
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zet(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zet(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 
 }
