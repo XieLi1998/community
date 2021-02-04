c0a187c4b65b32584ff787f457f69af12a59d664
xieli <2524546110@qq.com>
2021-02-04 15:47:55 +0800

    开发社区搜索功能

--- src/main/java/com/xieli/community/event/EventConsumer.java
+++ src/main/java/com/xieli/community/event/EventConsumer.java
@@ -1,8 +1,11 @@
 package com.xieli.community.event;
 
 import com.alibaba.fastjson.JSONObject;
+import com.xieli.community.entity.DiscussPost;
 import com.xieli.community.entity.Event;
 import com.xieli.community.entity.Message;
+import com.xieli.community.service.DiscussPostService;
+import com.xieli.community.service.ElasticsearchService;
 import com.xieli.community.service.MessageService;
 import com.xieli.community.util.CommunityConstant;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -27,6 +30,12 @@ public class EventConsumer implements CommunityConstant {
 
     @Autowired
     private MessageService messageService;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private ElasticsearchService elasticsearchService;
 
     @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
     public void handleCommentMessage(ConsumerRecord record) {
@@ -61,6 +70,24 @@ public class EventConsumer implements CommunityConstant {
 
         message.setContent(JSONObject.toJSONString(content));
         messageService.addMessage(message);
+    }
+
+    // 消费发帖事件
+    @KafkaListener(topics = {TOPIC_PUBLISH})
+    public void handlePublishMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息的内容为空！");
+            return;
+        }
+
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误！");
+            return;
+        }
+
+        DiscussPost post = discussPostService.findDiscussPostById(event.getEntityId());
+        elasticsearchService.saveDiscussPost(post);
     }
 
 }
