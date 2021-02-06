47511d75848677799dae757b56e3c09cf14b3cf4
xieli <2524546110@qq.com>
2021-02-06 23:49:00 +0800

    Spring Security

--- src/main/java/com/xieli/community/controller/HomeController.java
+++ src/main/java/com/xieli/community/controller/HomeController.java
@@ -68,6 +68,12 @@ public class HomeController implements CommunityConstant {
         return "/error/500";
     }
 
+    // 拒绝访问时的提示页面
+    @RequestMapping(path = "/denied", method = RequestMethod.GET)
+    public String getDeniedPage() {
+        return "/error/404";
+    }
+
     // ajax示例
     @RequestMapping(path = "/ajax", method = RequestMethod.POST)
     @ResponseBody
