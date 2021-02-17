9cacd13375f0fd9d3d87a83f78f51195c158a32f
xieli <2524546110@qq.com>
2021-02-17 23:37:06 +0800

    优化网站的性能

---
+++ src/test/java/com/xieli/community/CaffeineTests.java
@@ -0,0 +1,39 @@
+package com.xieli.community;
+
+import com.xieli.community.entity.DiscussPost;
+import com.xieli.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+/**
+ * Created by xieli on 2021/2/17.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService postService;
+
+    @Test
+    public void initDataForTest() {
+        for (int i = 0; i < 300000; i++) {
+            DiscussPost post = new DiscussPost();
+            post.setUserId(111);
+            post.setTitle("互联网求职暖春计划");
+            post.setContent("今年的就业形势，确实不容乐观。过了个年，仿佛跳水一般，整个讨论区哀鸿遍野！");
+            post.setCreateTime(new Date());
+            post.setScore(Math.random() * 2000);
+            postService.addDiscussPost(post);
+        }
+    }
+
+}
